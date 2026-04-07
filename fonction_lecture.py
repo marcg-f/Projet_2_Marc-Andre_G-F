a142c18cea720c8889f9be133cfd33a0ec1a10c6
marcg-f <marclefournier@gmail.com>
2026-04-07 10:50:29 -0400

    up

--- fonction_lecture.py
+++ fonction_lecture.py
@@ -7,21 +7,41 @@ ajd = ajd.replace("-", "")
 print(ajd)
 message.next_col()
 fichier = "calendrier_collecte.ics"
+k=0
+contenu_ligne=[] 
+line_no=0
+debut=[]
 with open(fichier, 'r') as file:
-    for line_no, line in enumerate(file, start=1):
-        debut = line.split(":")
-        if(debut[0] == "DTSTART"):
-            time = debut[1].split("T")
-            date = time[0]
-            if(date >= ajd):
-                bac_info = line[2+line_no].split(":")
-                bac_nom = bac_info[0]
-        deuxieme = line[13+line_no]
-        deuxieme = line.split(":")
+    line = file.readlines()
+    for line_no in line: 
+           
+        contenu_ligne=line[line_no].split(":")      
+        if(contenu_ligne[0] == "SUMMARY" and bac1_found == True):
+            bac1_nom = contenu_ligne[1]
+            bac2_trig = True
+            bac1_found = False
+        elif(contenu_ligne[0] == "SUMMARY" and bac2_found == True):
+            bac2_nom = contenu_ligne[1]
+            final = True
+            bac2_found = False
+            bac2_trig = False
+        elif(contenu_ligne[0] == "DTSTART" and bac2_trig == False):
+            bac1_time = contenu_ligne[1].split("T")
+            bac1_date = bac1_time[0]
+            if(bac1_date >= ajd):
+                bac1_found = True
+        elif(contenu_ligne[0] == "DTSTART" and bac2_trig == True):
+            bac2_time = contenu_ligne[1].split("T")
+            bac2_date = bac2_time[0]
+            if(bac2_date >= ajd):
+                bac2_found = True
+
+        deuxieme = contenu_ligne[13+k]
+        deuxieme = contenu_ligne.split(":")
         if(deuxieme[0] == "DTSTART"):
             time = deuxieme[1].split("T")
             date_2 = time[0]
             if(date_2 == date):
-                bac_info_2 = line[15+line_no].split(":")
+                bac_info_2 = contenu_ligne[15+k].split(":")
                 bac_nom_2 = bac_info_2[0]
 
