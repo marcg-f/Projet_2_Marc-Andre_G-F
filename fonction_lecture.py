cf09d078ce4178f9f0b16433ab6c81bc047692a4
marcg-f <marclefournier@gmail.com>
2026-04-08 18:17:36 -0400

    try&execpt

--- fonction_lecture.py
+++ fonction_lecture.py
@@ -6,7 +6,6 @@ def lecture_col():
     ajd = str(ajd)
     ajd = ajd.replace("-", "")
     ajd = int(ajd)
-
     message.next_col()
     fichier = "calendrier_collecte.ics"
     contenu_ligne = [] 
@@ -14,38 +13,48 @@ def lecture_col():
     bac2_trig = 0
     bac1_found = 0
     bac2_found = 0
-    with open(fichier, 'r') as file:
-
-        line = file.readlines()
-
-        for line_no in line:            
-            contenu_ligne = line_no.split(":")      
-            if(contenu_ligne[0] == "SUMMARY" and bac1_found == True):
-                bac1_nom = contenu_ligne[1]
-                bac2_trig = True
-                bac1_found = False
-            elif(contenu_ligne[0] == "SUMMARY" and bac2_found == True):
-                bac2_nom = contenu_ligne[1]
-                final = True
-                bac2_found = False
-                bac2_trig = False
-            elif(contenu_ligne[0] == "DTSTART" and bac2_trig == False):
-                bac1_time = contenu_ligne[1].split("T")
-                bac1_date = int(bac1_time[0])
-                if(bac1_date >= ajd):
-                    bac1_found = True
-            elif(contenu_ligne[0] == "DTSTART" and bac2_trig == True):
-                bac2_time = contenu_ligne[1].split("T")
-                bac2_date = int(bac2_time[0])
-                if(bac2_date >= ajd):
-                    bac2_found = True
-            elif(final == True) : break
-    file.close() 
-    delete = message.info_col(bac1_nom, bac2_nom)
-    if(delete == "O"):
-        with open(fichier, 'w') as file:
-            fichier.write("L'ancien fichier à été écrasé!\n")
+    try:
+    # On ESSAIE d'exécuter ce code
+        with open(fichier, "r"):
+            test = fichier.read()
+            path_found = True
         fichier.close()
+
+    except FileNotFoundError:
+        message.fichier_test()
+        path_found = False
+
+    if(path_found):
+        with open(fichier, 'r') as file:
+            line = file.readlines()
+            for line_no in line:            
+                contenu_ligne = line_no.split(":")      
+                if(contenu_ligne[0] == "SUMMARY" and bac1_found == True):
+                    bac1_nom = contenu_ligne[1]
+                    bac2_trig = True
+                    bac1_found = False
+                elif(contenu_ligne[0] == "SUMMARY" and bac2_found == True):
+                    bac2_nom = contenu_ligne[1]
+                    final = True
+                    bac2_found = False
+                    bac2_trig = False
+                elif(contenu_ligne[0] == "DTSTART" and bac2_trig == False):
+                    bac1_time = contenu_ligne[1].split("T")
+                    bac1_date = int(bac1_time[0])
+                    if(bac1_date >= ajd):
+                        bac1_found = True
+                elif(contenu_ligne[0] == "DTSTART" and bac2_trig == True):
+                    bac2_time = contenu_ligne[1].split("T")
+                    bac2_date = int(bac2_time[0])
+                    if(bac2_date >= ajd):
+                        bac2_found = True
+                elif(final == True) : break
+        file.close() 
+        delete = message.info_col(bac1_nom, bac2_nom)
+        if(delete == "O"):
+            with open(fichier, 'w') as file:
+                fichier.write("L'ancien fichier à été écrasé!\n")
+            fichier.close()
     return
     
 
--- message.py
+++ message.py
@@ -9,38 +9,41 @@ def menu():
             message = input("Entré inconnue! \nVoulez-vous acceder à votre calendrier de collecte de bacs, ou écrire un nouveau calendrier? \nA: Collecte de bacs \nB: Écrire un nouveau calendrier \nQ: Quitter \n" )
             message = message.upper()    
     return message
+
 def next_col():
     choix = input("Voulez-vous voir si la prochaine collecte est celle des déchets, du composte ou recyclage? O/N\n")
     choix = choix.upper()
     while(choix != "O" and choix != "N"):
         choix = input("Entrée inconnue! \nVoulez-vous voir si la prochaine collecte est celle des déchets, du composte ou recyclage? O/N\n")
-        choix = choix.upper()
- 
+        choix = choix.upper() 
     return choix
 
 def info_col(bac1_nom, bac2_nom):
     anwser = input(F"La prochaine collecte du mercredi sera le bac de {bac1_nom} et de {bac2_nom} \nVoulez-vous envoyer le calendrier au bac à recyclage? O/N")
     anwser = anwser.upper()
-    while(anwser != "O" or anwser != "N"):
+    while(anwser != "O" and anwser != "N"):
         anwser = input("Entrée inconnue!\nVoulez-vous envoyer le calendrier au bac à recyclage? O/N\n")
         anwser = anwser.upper()  
     return anwser
 
 def titre_fichier():
-    titre = input("Quelle titre voulez-vous pour votre fichier txt?")
-    
+    titre = input("Quelle titre voulez-vous pour votre fichier txt?")    
     while(titre == False):
         titre = input("Le fichier doit posséder un titre!\n")
     return titre
 
 def contenu_txt():
-    texte = input("Entrer le texte à inserer dans le fichier:\n")
+    texte = input("Entrer le texte à inserer dans le fichier:\n") # Rien entrer crée un fichier vide donc aucune entrer sécurisée necessaire
     return texte
 
 def view_txt():
     anwser = input("Voulez-vous consulter votre fichier nouvellement créé? (O/N)")
     anwser = anwser.upper()
-    while(anwser != "O" or anwser != "N"):
+    while(anwser != "O" and anwser != "N"):
         anwser = input("Entrée inconnue!\nVoulez-vous consulter votre fichier nouvellement créé? (O/N)\n")
         anwser = anwser.upper() 
     return anwser
+
+def fichier_test():
+    print("Le fichier est introuvable!\nRetour au menu principal!")
+    return
