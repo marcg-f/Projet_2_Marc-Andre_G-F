3005e318bebfd0442e3ba2866357b154d6b50d5e
marcg-f <marclefournier@gmail.com>
2026-04-09 19:19:28 -0400

    deluxe_prenium

--- main_traitement_fichier.py
+++ main_traitement_fichier.py
@@ -2,7 +2,7 @@
 **********************************************
 Projet: 2
 Auteur: Marc-André Grondin-Fournier
-But du projet: Programme qui cherche dans un calendrier les deux prochains bacs à mettre au chemin et qui permet aussi la création/lecture d'un fichier txt
+But du projet: Programme qui cherche dans un calendrier et qui permet aussi la création/lecture d'un fichier txt
 Date: 2026/04/08
 Nom du fichier: main_traitement_fichier.py
 **********************************************
